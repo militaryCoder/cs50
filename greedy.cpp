71d28572ba01c4f248133517eb133d190ff54397
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-21 22:12:38 +0300

    refactoring;

--- greedy.cpp
+++ greedy.cpp
@@ -2,119 +2,104 @@
 #include <cstring>
 #include <iostream>
 #include <regex>
+#include <exception>
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::regex;
+const uint32_t DECIMAL_POWER_BASE = 10u;
 
-float getFloat()
+uint8_t charToInteger(char value)
 {
-	float finalValue = 0;
-	
-	std::string buffer;
-		
-	bool isValueFormatCorrect = false;
-	regex* correctValueFormat = new regex("[\\d]+[\\.]?[\\d]{0,2}");
+    if (value <= '0' && value >= '9')
+    {
+        throw std::out_of_range("Invalid character");
+    }
 
-	do
-	{
-		getline(cin, buffer);
-		isValueFormatCorrect = std::regex_match(buffer, *correctValueFormat);
-		
-		if (isValueFormatCorrect)
-		{
-			cout << "Format correct" << endl;
-		}
-		else
-		{
-			cout << "Format incorrect, please try again" << endl;
-		}
-	} while (!isValueFormatCorrect);
+    return (value - '0');
+}
 
-	delete correctValueFormat;
-	correctValueFormat = nullptr;
+float parseFloat(std::string buffer)
+{
+    float finalValue = 0;
+    
+    std::regex correctValueFormat(R"(\d+(\.\d+)?)");
 
-	int dotIndex;	
-	for (int i = 0; i < buffer.size(); i++)
-	{
-		if (buffer[i] == '.')
-		{
-			dotIndex = i;
-			break;
-		}
-	}
-	
-	for (int i = 0; i < dotIndex; i++)
-	{
-		int decPower = dotIndex - (i + 1);
-		bool isDigit = false;
+    // do
+    // {
+    // 	getline(cin, buffer);
+    // 	isValueFormatCorrect = std::regex_match(buffer, *correctValueFormat);
+        
+    // 	if (isValueFormatCorrect)
+    // 	{
+    // 		cout << "Format correct" << endl;
+    // 	}
+    // 	else
+    // 	{
+    // 		cout << "Format incorrect, please try again" << endl;
+    // 	}
+    // } while (!isValueFormatCorrect);
 
-		regex* digits = new regex("\\d");
-		isDigit = std::regex_match(std::string(1, buffer[i]), *digits);
+    if (!std::regex_match(buffer, correctValueFormat))
+    {
+        throw std::invalid_argument("Invalid string");
+    }
 
-		delete digits;
-		digits = nullptr;
+    const int dotIndex = buffer.find('.');
+    const int integerPartEndIndex = (std::string::npos == dotIndex) ? buffer.size() : dotIndex;
 
-		if (isDigit)
-		{
-			float digitValue = ((int)buffer[i] - (int)'0') * pow(10, decPower);
-			finalValue += digitValue;
-		}
-	}
-	
-	for (int i = (dotIndex + 1); i < buffer.size(); i++)
-	{
-		if (buffer[i] == ' ')
-		{
-			continue;
-		}
-		
-		float decPower = dotIndex - i;
-		
-		bool isDigit = false;
+    for (int i = 0; i < integerPartEndIndex; i++)
+    {
+        finalValue += charToInteger(buffer[i]) * pow(DECIMAL_POWER_BASE, dotIndex - (i + 1));
+    }
 
-		regex* digits = new regex("\\d");
-		isDigit = std::regex_match(std::string(1, buffer[i]), *digits);
+    if (std::string::npos != dotIndex)
+    {
+        for (int i = (dotIndex + 1); i < buffer.size(); i++)
+        {
+            finalValue += charToInteger(buffer[i]) * pow(DECIMAL_POWER_BASE, dotIndex - i);
+        }
+    }
 
-		delete digits;
-		digits = nullptr;
-		
-		if (isDigit)
-		{
-			float digitValue = ((int)buffer[i] - (int)'0') * pow(10, decPower);
-			finalValue += digitValue;
-		}
-	}
-
-	return finalValue;
+    return finalValue;
 }
 
 int main()
 {
-	const int coins[4] = { 1, 5, 10, 25 };
-	
-	float input = getFloat();
-	int cents = input * 100;
-	
-	int totalCoins = 0;
-	while (cents != 0)
-	{
-		int n = cents / coins[0];
-		int currentCoinValue = 0;
-		for (int c = 1; c < 4; c++)
-		{
-			if ((cents / coins[c]) < n
-				&& (cents / coins[c]) > 0)
-			{
-				n = cents / coins[c];
-				currentCoinValue = c;
-			}
-			
-		}
-		totalCoins += n;
-		cents -= coins[currentCoinValue] * n;
-	}
-	cout << totalCoins << endl;
-	return 0;
+    const int coinsNominals[4] = { 1, 5, 10, 25 };
+    
+    try
+    {
+        std::string inputString;
+        std::getline(std::cin, inputString);
+
+        float input = parseFloat(inputString);
+
+        uint64_t cents = static_cast<uint64_t>(input * 100);
+        
+        int totalCoins = 0;
+        while (cents != 0)
+        {
+            int numberOfCoins = cents / coinsNominals[0];
+            int currentCoinIndex = 0;
+
+            for (int c = 1; c < 4; c++)
+            {
+                if ((cents / coinsNominals[c]) < numberOfCoins &&
+                    (cents / coinsNominals[c]) > 0)
+                {
+                    numberOfCoins = cents / coinsNominals[c];
+                    currentCoinIndex = c;
+                }
+            }
+
+            totalCoins += numberOfCoins;
+            cents -= coinsNominals[currentCoinIndex] * numberOfCoins;
+        }
+
+        std::cout << totalCoins << std::endl;
+    }
+    catch (std::invalid_argument &invalidArgumentException)
+    {
+        std::cout << "User entered invalid value" << std::endl;
+    }
+    
+    return 0;
 }
