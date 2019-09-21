cc94b322bfde5ca38725372d86fe2def6e65b00f
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-21 22:12:09 +0300

    added some flags;

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "enter file name is needed to compile"
 read BUILD_TARGET
 echo "building ${BUILD_TARGET} ..."
 
-clang++ -o bin/${BUILD_TARGET[@]//.cpp/} ${BUILD_TARGET}
+clang++ -O2 -Werror -o bin/${BUILD_TARGET[@]//.cpp/} ${BUILD_TARGET}
 
 echo "${BUILD_TARGET} was built"
