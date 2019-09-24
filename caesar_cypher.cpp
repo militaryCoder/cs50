75d02c4be65011a8ae307357292ccb7fb84f23ac
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-25 01:59:58 +0300

    added method which calculates letter position after cyphering; added getting cypher key from command line arguments; not finished yet: exception handling

--- caesar_cypher.cpp
+++ caesar_cypher.cpp
@@ -1,9 +1,38 @@
 #include <cstring>
+#include <exception>
 #include <iostream>
  
-int main()
+const uint8_t CYPHER_SHIFT_KEY_ARGUMENT_INDEX = 1;
+
+const uint8_t ALPHABET_ASCII_STARTING_POSITION = 'A' - 1;
+const uint8_t ALPHABET_LENGTH = 26;
+
+uint8_t calculateCypheredLetterPosition(uint8_t letter, uint32_t key)
 {
-	std::string inputString;
-	std::getline(std::cin, inputString);
+    return (((letter - ALPHABET_ASCII_STARTING_POSITION) + key) % ALPHABET_LENGTH) + ALPHABET_ASCII_STARTING_POSITION;
+}
+
+int main(int argc, char argv[])
+{
+    try
+    {
+        const uint32_t cypherShiftKey = (uint32_t)argv[CYPHER_SHIFT_KEY_ARGUMENT_INDEX];
+
+        std::string inputString;
+        std::getline(std::cin, inputString);
+
+        std::string outputString[inputString.size()];
+        
+        for (size_t i = 0; i < inputString.size(); i++)
+        {
+            outputString[i] = calculateCypheredLetterPosition(inputString[i], cypherShiftKey);
+        }
+        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
 }
