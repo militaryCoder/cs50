53efb3afc265b923a01ec711e380909bc7210f1f
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-24 18:35:28 +0300

    added new solution

---
+++ initials.cpp
@@ -0,0 +1,22 @@
+#include <cstring>
+#include <iostream>
+
+int main()
+{
+    std::string inputName;
+    std::getline(std::cin, inputName);
+
+    for (uint8_t i = 0; i < inputName.size(); i++)
+    {
+        for (uint32_t character = 'A'; character <= 'Z'; character++)
+        {
+            if (character == inputName[i])
+            {
+                std::cout << character;
+            }
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
