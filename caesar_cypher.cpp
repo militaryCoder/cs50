e45fbbb5e005f77e1b93a6d0f7f6f90a781cf32a
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-25 10:47:52 +0300

    added exception handling; added validation of entered string; ToFix: ignoring std::getline statement

--- caesar_cypher.cpp
+++ caesar_cypher.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <exception>
 #include <iostream>
+#include <regex>
  
 const uint8_t CYPHER_SHIFT_KEY_ARGUMENT_INDEX = 1;
 
@@ -12,26 +13,45 @@ uint8_t calculateCypheredLetterPosition(uint8_t letter, uint32_t key)
     return (((letter - ALPHABET_ASCII_STARTING_POSITION) + key) % ALPHABET_LENGTH) + ALPHABET_ASCII_STARTING_POSITION;
 }
 
-int main(int argc, char argv[])
+//Uncomment main() to use feature from line 25
+int main(/* int argc, char** argv */)
 {
     try
     {
-        const uint32_t cypherShiftKey = (uint32_t)argv[CYPHER_SHIFT_KEY_ARGUMENT_INDEX];
+    	/*
+    	Taking cypher key value directly from command line program execution arguments
+    	Does not work on mobile devices
+    	*/
+        //const uint64_t cypherShiftKey = (uint64_t)argv[CYPHER_SHIFT_KEY_ARGUMENT_INDEX];
+
+		uint32_t cypherShiftKey;
+		std::cout << "1";
+		std::cin >> cypherShiftKey;
 
         std::string inputString;
+        std::cout << "2";
         std::getline(std::cin, inputString);
-
-        std::string outputString[inputString.size()];
         
-        for (size_t i = 0; i < inputString.size(); i++)
+        std::regex inputTemplate("(\\w)+");
+        
+        if (std::regex_match(inputString, inputTemplate))
         {
-            outputString[i] = calculateCypheredLetterPosition(inputString[i], cypherShiftKey);
-        }
+        	std::string outputString[inputString.size()];
         
+        	for (size_t i = 0; i < inputString.size(); i++)
+        	{
+            	outputString[i] = calculateCypheredLetterPosition(inputString[i], cypherShiftKey);
+        	}
+        	std::cout << outputString << std::endl;
+        }
+        else
+        {
+        	throw std::invalid_argument("Entered string that did not matched the template");
+        }
     }
-    catch(const std::exception& e)
+    catch(const std::invalid_argument& exc)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << exc.what() << '\n';
     }
 
     return 0;
