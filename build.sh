5c0cf7ff482e52bb0ef7eb719317292fabcd0c8e
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-25 20:36:52 +0300

    file which is to be compiled now given as a command line argument

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "enter file name is needed to compile"
-read BUILD_TARGET
+BUILD_TARGET=$1
+
 echo "building ${BUILD_TARGET} ..."
 
 clang++ -O2 -Werror -o bin/${BUILD_TARGET[@]//.cpp/} ${BUILD_TARGET}
