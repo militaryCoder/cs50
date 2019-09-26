e3b0988bd8183e7a256e1132879418ff06333382
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-26 23:34:09 +0300

    finalized character encryption method, test provided

--- modular_tests/character_encryption.cpp
+++ modular_tests/character_encryption.cpp
@@ -1,19 +1,43 @@
+#include <exception>
 #include <iostream>
 
-const uint8_t ALPHABET_ASCII_UPPERCASE_STARTING_POSITION = 'A' - 1;
 const uint8_t ALPHABET_LENGTH = 26;
 
-uint8_t calculateShiftedLetterPosition(uint8_t letter, uint32_t shift)
+uint8_t calculateShiftedLetterPosition(uint8_t letter, uint32_t shiftKey)
 {
-    return letter + shift;
+    if (('A' <= letter && 'Z' >= letter) ||
+        ('a' <= letter && 'z' >= letter))
+    {
+        const uint8_t alphabetASCIIStarterPosition = ('Z' >= letter) ? ('A' - 1) : ('a' - 1);
+
+        const uint8_t classicLetterAlphabeticPosition = letter - alphabetASCIIStarterPosition;
+        const uint8_t shiftedLetterPosition = (classicLetterAlphabeticPosition + shiftKey) % 26;
+        const uint8_t finalLetterPosition = shiftedLetterPosition + alphabetASCIIStarterPosition;
+
+        return finalLetterPosition;
+    }
+    else
+    {
+        throw std::invalid_argument("Invalid character provided");
+    }
 }
+
 int main()
 {
-    const int shift = 2;
-    const char letter = 'A';
+    const uint32_t shift = 2;
+    
 
-    char finalCharacter = calculateShiftedLetterPosition(letter, shift);
-    std::cout << finalCharacter << std::endl;
+    try
+    {
+        char letter;
+        std::cin >> letter;
+        char finalCharacter = calculateShiftedLetterPosition(letter, shift);
+        std::cout << finalCharacter << std::endl;
+    }
+    catch (const std::invalid_argument& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
