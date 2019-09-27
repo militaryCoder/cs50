8702d9f5ec2780be9d58c93fdebacc69b1d141d0
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-09-27 20:19:02 +0300

    Tabs to spaces convertation

--- modular_tests/command_line_argument.cpp
+++ modular_tests/command_line_argument.cpp
@@ -4,10 +4,10 @@ const uint8_t COMMAND_LINE_ARGUMENT_INDEX = 1;
 
 int main(int argc, char *argv[])
 {
-	if (NULL != argv[1])
-	{
-		std::cout << argv[1] << std::endl;
-	}
-	
-	return 0;
+    if (NULL != argv[1])
+    {
+        std::cout << argv[1] << std::endl;
+    }
+
+    return 0;
 }
