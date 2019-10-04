a1348a1e20abec6ee5b133ebbc3c01217cd6dec4
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-10-05 00:54:30 +0300

    separated character encryption method into independent header

---
+++ include/encryption_methods.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstdint>
+
+uint8_t calculateEncryptedLetterPosition(uint8_t letter, uint32_t shiftKey);
---
+++ src/encryption_methods.cpp
@@ -0,0 +1,30 @@
+#include "../include/encryption_methods.hpp"
+
+#include <stdexcept>
+
+
+uint8_t calculateEncryptedLetterPosition(uint8_t letter, uint32_t shiftKey)
+{
+    const uint8_t alphabetLength = 26;
+    const uint8_t space = ' ';
+
+    if (space == letter)
+    {
+        return letter;
+    }
+
+    if (('A' <= letter && 'Z' >= letter) ||
+        ('a' <= letter && 'z' >= letter))
+    {
+        const uint8_t alphabetASCIIStarterPosition = ('Z' >= letter) ? ('A' - 1) : ('a' - 1);
+
+        const uint8_t classicLetterAlphabeticPosition = letter - alphabetASCIIStarterPosition;
+        const uint8_t shiftedLetterPosition = (classicLetterAlphabeticPosition + shiftKey) % alphabetLength;
+
+        return (shiftedLetterPosition + alphabetASCIIStarterPosition);
+    }
+    else
+    {
+        throw std::invalid_argument("Invalid character provided");
+    }
+}
