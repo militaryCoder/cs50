26b89e912272762260e98293ae4c7f1d09f6d5ef
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-10-05 22:54:56 +0300

    added library using

--- src/cyphers/caesar_cypher.cpp
+++ src/cyphers/caesar_cypher.cpp
@@ -1,43 +1,21 @@
+#include "../../include/encryption_methods.hpp"
+
 #include <cstring>
 #include <exception>
 #include <iostream>
 #include <regex>
- 
-const uint8_t CYPHER_SHIFT_KEY_ARGUMENT_INDEX = 1;
 
-uint8_t calculateEncryptedLetterPosition(uint8_t letter, uint32_t shiftKey)
-{
-    const uint8_t alphabetLength = 26;
-    const uint8_t space = ' ';
-
-    if (space == letter)
-    {
-        return static_cast<uint8_t>(letter);
-    }
-
-    if (('A' <= letter && 'Z' >= letter) ||
-        ('a' <= letter && 'z' >= letter))
-    {
-        const uint8_t alphabetASCIIStarterPosition = 'Z' >= letter ? 'A' - 1 : 'a' - 1;
-
-        const uint8_t classicLetterAlphabeticPosition = letter - alphabetASCIIStarterPosition;
-        const uint8_t shiftedLetterPosition = (classicLetterAlphabeticPosition + shiftKey) % alphabetLength;
-
-        return shiftedLetterPosition + alphabetASCIIStarterPosition;
-    }
-    
-    throw std::invalid_argument("Invalid character provided");
-    
-}
+const uint8_t CYPHER_SHIFT_KEY_ARGUMENT_INDEX = 1u;
 
 int main(int argc, char** argv)
 {
     try
     {
-    if (argc < 2)
+        if (argc < 2)
         {
             throw std::invalid_argument("No cypher key provided");
         }
+
         const uint32_t commandLineArgument = static_cast<uint32_t>(*argv[CYPHER_SHIFT_KEY_ARGUMENT_INDEX]);
 
         if (0 != commandLineArgument)
