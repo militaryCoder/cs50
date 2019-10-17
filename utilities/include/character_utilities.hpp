8dd16e16f3c56a5729bc5e286b3439e822f255dd
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-10-18 00:19:29 +0300

    Decided to use header-guard instead of #pragma once

--- utilities/include/character_utilities.hpp
+++ utilities/include/character_utilities.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef character_utilities_h
+#define character_utilities_h
 
 #include <cstdint>
 
@@ -6,5 +7,7 @@ namespace charutils
 {
     bool isLetter(uint8_t character);
     bool isUpperCase(uint8_t character);
-    bool isDownCase(uint8_t character);
+    bool isLowerCase(uint8_t character);
 } // namespace char_utils
+
+#endif // character_utilities_h
