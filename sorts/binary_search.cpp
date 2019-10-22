82af12f153ad31496f548eccf998972db00efcd8
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2019-10-22 23:26:11 +0300

    changed brace style to Stroustrup; std::vector now given as ref; added cmd line parsing

--- sorts/binary_search.cpp
+++ sorts/binary_search.cpp
@@ -2,21 +2,25 @@
 #include <iostream>
 #include <vector>
 
-const int32_t binarySearch(const int32_t value, const std::vector<int32_t> vector, const size_t size) {
-    if (size < 0) {
+const int32_t binarySearch(const int32_t value, const std::vector<int32_t> &vector, const size_t size) {
+    if (size < 0)
+    {
         throw std::invalid_argument("Given argument [size] is invalid - cannot be less than minimal treshold");
     }
 
     uint32_t currentAccessIndex = size / 2;
     std::cout << "Starting at position [" << currentAccessIndex << "]\n";
     
-    while (vector[currentAccessIndex] != value) {
+    while (vector[currentAccessIndex] != value)
+    {
         const int32_t accessedValue = vector[currentAccessIndex];
         std::cout << "Accessed value [" << accessedValue << "] at index [" << currentAccessIndex << "]\n";
-        if (value > accessedValue) {
+        if (value > accessedValue)
+        {
             currentAccessIndex = currentAccessIndex + ((size - currentAccessIndex) / 2);
         }
-        else if (value < accessedValue) {
+        else if (value < accessedValue)
+        {
             currentAccessIndex = currentAccessIndex - ((1 == currentAccessIndex) ? 1 : currentAccessIndex / 2);
         }
     }
@@ -24,24 +28,35 @@ const int32_t binarySearch(const int32_t value, const std::vector<int32_t> vecto
     return currentAccessIndex;
 }
 
-int main() {
+int main(int argc, char **argv)
+{
     int32_t inputValueIndex = 0;
     
-    try {
-        std::ifstream input("input.txt");
+    try
+    {
+        if (argc < 3)
+        {
+            throw std::invalid_argument("Usage: ./binary_search <path-to-source> <search-query>");
+        }
+        
+        const std::string pathToSource = argv[1];
+        const int32_t inputValue = static_cast<int32_t>(*argv[2] - '0');
+        std::cout << inputValue << std::endl;
+
+        std::ifstream input(pathToSource);
 
         std::vector<int32_t> inputVector;
 
         int32_t fstreamBuffer = 0;
-        while (input >> fstreamBuffer) {
+        while (input >> fstreamBuffer)
+        {
             inputVector.push_back(fstreamBuffer);
         }
 
-        int32_t inputValue;
-        std::cin >> inputValue;
-
         inputValueIndex = binarySearch(inputValue, inputVector, inputVector.size());
-    } catch (std::invalid_argument &exc) {
+    }
+    catch (std::invalid_argument &exc)
+    {
         std::cerr << exc.what() << std::endl;
     }
 
