12b1aab2075666ed4a7c56977cbd3725cd928707
RostislavTolushkin <rostislav.tolushkin@gmail.com>
2019-10-24 22:06:23 +0300

    Added providence of msg and keyword from cmd line

--- cyphers/vigener_cypher.cpp
+++ cyphers/vigener_cypher.cpp
@@ -2,38 +2,46 @@
 
 #include <cstring>
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char **argv)
 {
-    std::string inputString;
-    std::string keyword;
-
-    std::cout << "Enter string to be encrypted: ";
-    std::getline(std::cin, inputString);
-    std::cout << "Enter keyword: ";
-    std::getline(std::cin, keyword);
-    
-    const size_t inputStringLength = inputString.size();
-    char *outputString = new char[inputStringLength];
-
-    const char space = ' ';
-    const size_t keywordLength = keyword.size();
-
-    uint32_t spaceCount = 0;
-
-    for (size_t i = 0; i < inputStringLength; i++)
+    try
     {
-        if (space == inputString[i])
+        if (argc < 3)
         {
-            spaceCount++;
+            throw std::invalid_argument("Usage: ./vigener_cypher <message> <keyword>");
         }
-        uint64_t keywordCurrentCharacterIndex = (i - spaceCount) % keywordLength;
+        const std::string inputString = argv[1];
+        const std::string keyword = argv[2];
+        
+        const size_t inputStringLength = inputString.size();
+        char *outputString = new char[inputStringLength];
 
-        const uint32_t shiftValue = static_cast<uint32_t>(keyword[keywordCurrentCharacterIndex]);
+        const char space = ' ';
+        const size_t keywordLength = keyword.size();
 
-        outputString[i] = encr_methods::calculateEncryptedLetterPosition(static_cast<uint8_t>(inputString[i]), shiftValue);
+        uint32_t spaceCount = 0;
+
+        for (size_t i = 0; i < inputStringLength; i++)
+        {
+            if (space == inputString[i])
+            {
+                spaceCount++;
+            }
+            uint64_t keywordCurrentCharacterIndex = (i - spaceCount) % keywordLength;
+
+            const uint32_t shiftValue = static_cast<uint32_t>(keyword[keywordCurrentCharacterIndex]);
+
+            outputString[i] = encr_methods::calculateEncryptedLetterPosition(static_cast<uint8_t>(inputString[i]), shiftValue);
+        }
+
+        std::cout << outputString << std::endl;
     }
-
-    std::cout << outputString << std::endl;
+    catch (const std::invalid_argument &exc)
+    {
+        std::cout << exc.what() << std::endl;
+    }
+    
     return 0;
 }
