b537455a86e8cd48a5872332a235a489295da65a
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2021-12-21 20:49:38 +0300

    refactored caesar cypher A LOT

--- cyphers/caesar_cypher.cpp
+++ cyphers/caesar_cypher.cpp
@@ -1,54 +1,37 @@
 #include "encryption_methods.hpp"
 
-#include <cstring>
+#include <cstdlib>
 #include <exception>
 #include <iostream>
-#include <regex>
+#include <string>
 
 const uint8_t CYPHER_SHIFT_KEY_ARGUMENT_INDEX = 1u;
 
-int main(int argc, char** argv)
-{
-    try
-    {
-        if (argc < 2)
-        {
-            throw std::invalid_argument("No cypher key provided");
+int main(int argc, char** argv) {
+    try {
+        if (argc < 2) {
+            throw std::invalid_argument("No cypher key provided.");
         }
 
-        const uint32_t commandLineArgument = static_cast<uint32_t>(*argv[CYPHER_SHIFT_KEY_ARGUMENT_INDEX]);
+        const uint32_t cypherShiftKey = std::atoi(argv[CYPHER_SHIFT_KEY_ARGUMENT_INDEX]);
 
-        if (0 != commandLineArgument)
-        {
-            const uint32_t cypherShiftKey = commandLineArgument - '0';
-            std::cout << "Cypher key: " << cypherShiftKey << std::endl;
-
+        if (0 != cypherShiftKey) {
             std::string inputString;
+            std::cout << "Enter string that should be encrypted.\n";
             std::getline(std::cin, inputString);
-            const size_t inputStringSize = inputString.size();
 
-            char *outputString = new char[inputStringSize + 1];
+            std::string outStr;
 
-            for (size_t i = 0; i < inputStringSize; i++)
-            {
-                uint8_t inputCharacter = static_cast<uint8_t>(inputString[i]);
-		char encryptedLetter = static_cast<char>(encr_methods::calculateEncryptedLetterPosition(inputCharacter, cypherShiftKey);
-
-                outputString[i] = encryptedLetter;
+            for (const auto &ch : inputString) {
+                    outStr += std::isalpha(ch) ? static_cast<char>(encr_methods::calculateEncryptedLetterPosition(ch, cypherShiftKey))
+                        : ch;
             }
             
-            outputString[inputStringSize] = '\0';
-            std::cout << outputString << std::endl;
+            std::cout << outStr << std::endl;
         }
-        else
-        {
-            throw std::invalid_argument("No cypher key provided");
-        }
-        
-        
+        else throw std::invalid_argument("Cypher should be more than zero.");
     }
-    catch (const std::invalid_argument& exc)
-    {
+    catch (const std::invalid_argument &exc) {
         std::cerr << exc.what() << '\n';
     }
 
